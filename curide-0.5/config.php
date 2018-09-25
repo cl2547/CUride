bf19690ad1448b03b6c26da6c7f230472a16512e
SharickXiang <42632162+SharickXiang@users.noreply.github.com>
2018-09-25 02:49:07 -0400

    Add config.php without passwords

---
+++ curide-0.5/config.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$DEBUG = 0;
+
+
+
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+$datatablename = "";
+$infotablename = "";
+$rootpath = "";
+
+
+/* modified 2018/09/03 - US EST */
+/* Information -- not to be changed on both servers. */
+$face = "index.php";
+$actionface = "action_face.php";
+$board = "board.php";
+$actionboard = "dbaction.php";
+
+
+
+
+?>
